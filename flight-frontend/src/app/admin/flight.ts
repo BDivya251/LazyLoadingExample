7558b54de9e0b813ef9a1c35b76154c1b0ed72ad
Divya1-b <mail2divya.bathini@gmail.com>
2025-12-14 21:57:41 +0530

    added

--- flight-frontend/src/app/admin/flight.ts
+++ flight-frontend/src/app/admin/flight.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { FlightWrapper } from './flight-wrapper';
-const FLIGHT_API='http://localhost:8123/flight-service/flight/airline'
+const FLIGHT_API='http://localhost:8123/flight-service/flight'
 // http://localhost:8123/flight-service/flight/airline/inventary/add
 @Injectable({
   providedIn: 'root',
@@ -11,13 +11,13 @@ export class FlightService {
   constructor(private http: HttpClient){}
 
   addAirline(data: any): Observable<any> {
-      return this.http.post(`${FLIGHT_API}/add`, data, { responseType: 'text' ,  headers: {
+      return this.http.post(`${FLIGHT_API}/airline/add`, data, { responseType: 'text' ,  headers: {
         'Content-Type': 'application/json'
       }});
     }
 
     addFlightInventory(data:any):Observable<any>{
-      return this.http.post(`${FLIGHT_API}/inventary/add`,data,
+      return this.http.post(`${FLIGHT_API}/airline/inventary/add`,data,
         {
            observe: 'response',   
         headers:{
@@ -26,13 +26,14 @@ export class FlightService {
       })
     }
 
-    searchFlights(departure: string, arrival: string) {
+    searchFlights( arrival: string,departure: string) {
   return this.http.get<FlightWrapper[]>(
     `${FLIGHT_API}/search`,
     {
       params: {
+        
+        arrival: arrival,
         departure: departure,
-        arrival: arrival
       }
     }
   );
